bd449ab4f1e937223152819867113a8fefc6e1a4
kyorohiro <kyorohiro@gmail.com>
2013-01-01 19:56:28 +0900

    creating composing text sample

--- KyoroSampleSurfaceViewEditor/src/info/kyorohiro/samples/android/test/CheckForComposingText.java
+++ KyoroSampleSurfaceViewEditor/src/info/kyorohiro/samples/android/test/CheckForComposingText.java
@@ -18,26 +18,69 @@ public class CheckForComposingText extends
 		MainActivity mainActivity = getActivity();
 		getInstrumentation().waitForIdleSync();
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(300);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 
-    	android.util.Log.v("kiyo","kick showInputConnection");
+    	android.util.Log.v("kiyo","senario:kick showInputConnection");
 		Editor editor = mainActivity.getEditor();
-		editor.showInputConnection();
-		getInstrumentation().waitForIdleSync();
 		try {
-			Thread.sleep(3000);
+			for(int j=0;j<50;j++) {
+				editor.showInputConnection();
+				getInstrumentation().waitForIdleSync();
+				IMESample ime = IMESample.getInstance();
+				if(ime != null) {
+					break;
+				}
+				Thread.sleep(100);
+			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 
-    	android.util.Log.v("kiyo","kick inputCommitText");
+		//
+		// setComposingText
+		//
+    	android.util.Log.v("kiyo","senario:kick inputComposingText");
 		IMESample ime = IMESample.getInstance();
-		ime.inputCommitText("おなかがすいた", 1);
+		ime.inputComposingText("おなかがすいた", 1);
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		//
+		// hideInputConnection
+		//
+    	android.util.Log.v("kiyo","senario:kick hideInputConnection");
+		editor.hideInputConnection();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		//
+		// setComposingText
+		//
+    	android.util.Log.v("kiyo","senario:kick inputComposingText");
+		ime = IMESample.getInstance();
+		ime.inputComposingText("おなかがすいた", 1);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		//
+		// commitConnection
+		//
+    	android.util.Log.v("kiyo","senario:kick inputCommitText");
+		ime.inputCommitText("お腹が空いた", 1);
+		try {
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
